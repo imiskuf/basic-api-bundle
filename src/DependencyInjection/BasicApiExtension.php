ec3a9f39a4154baab837da0a1614f1e6ab13c97d
ivan <ivan.miskuf@gmail.com>
2019-05-11 23:44:14 +0200

    extension rename

--- src/DependencyInjection/BasicApiExtension.php
+++ src/DependencyInjection/BasicApiExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class BasicApiBundleExtension extends Extension
+class BasicApiExtension extends Extension
 {
     /**
      * @param array $configs
