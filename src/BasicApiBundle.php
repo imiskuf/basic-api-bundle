d7fd83ac12c7c3dedc70d94f4ae513490a87e0d3
ivan <ivan.miskuf@gmail.com>
2019-05-11 14:52:57 +0200

    namespace updates

--- src/BasicApiBundle.php
+++ src/BasicApiBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BasicApi;
+namespace Imiskuf\BasicApiBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
--- src/DependencyInjection/BasicApiBundleExtension.php
+++ src/DependencyInjection/BasicApiBundleExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class BasicApiExtension extends Extension
+class BasicApiBundleExtension extends Extension
 {
     /**
      * @param array $configs
