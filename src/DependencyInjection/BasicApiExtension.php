07dfa9db565960a930b2232ca8a4c3402e812797
ivan <ivan.miskuf@gmail.com>
2019-05-11 12:51:59 +0200

    bundle configuration

---
+++ src/DependencyInjection/BasicApiExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BasicApi\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class BasicApiExtension extends Extension
+{
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../../config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
